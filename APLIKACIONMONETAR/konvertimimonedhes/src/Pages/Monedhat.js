8358e514613788fd3dc0d8f2e4013cf609d5ca75
Leke Tushaj <lek.tushaj2022@gmail.com>
2022-04-12 15:51:46 +0200

    Projekti ne REACT

--- APLIKACIONMONETAR/konvertimimonedhes/src/Pages/Monedhat.js
+++ APLIKACIONMONETAR/konvertimimonedhes/src/Pages/Monedhat.js
@@ -1,21 +1,23 @@
 import React, {useState, useEffect} from 'react'
 import './Monedhat.css'
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 //function Countries () {
  //   const [countries, setCountries] = useState([]);
 
   const Monedhat =  () => {
     const [countries, setCountries] = useState([]);
+    const [searchWord, setSearchWord] = useState("")
 
     useEffect(() => {
         axios({
             method: "GET",
 
-            url: "https://free.currconv.com/api/v7/countries?q=&compact=ultra&apiKey=f15baf939bd8527ca77d ",
+            url: `https://free.currconv.com/api/v7/countries?q=&compact=ultra&apiKey=f15baf939bd8527ca77d`,
         })
         .then((response) =>{
-        //   console.log(Object.values(response.data.results));
+        //  console.log(Object.values(response.data.results));
             setCountries(Object.values(response.data.results));
             console.log(countries)
         })
@@ -25,23 +27,36 @@ import axios from 'axios';
     },[]);
 
 
+    const filteredCountries = countries.filter((country) => {
+        return country.name.toLowerCase().includes(searchWord.toLowerCase());
+    })
+
   return (
     
     <>
+        <section className='container searchInp'>
+            <h3 className='inputitle'>Njësitë Monetare sipas Shteteve</h3> 
+            <input className='inputField' type="text" placeholder="emri shtetit" onChange={(event) => {
+                setSearchWord(event.target.value);
+                }}
+                />
+        </section>
         <section className='container grid'>
-            {countries.map(({name,id,alpha3,currencyName,currencyId,currencySymbol}) => (
+            {filteredCountries.map(({name,id,alpha3,currencyName,currencyId,currencySymbol}) => (
+             //   <Link to={`/${name}`} key={name}>
                             <article key={name}>
                                  <div className='details'>
                                      <div>
                                         <h3> {name} </h3>
                                          <h4>id: <span> {id} </span> </h4>
                                          <h4>shkShteterore: <span> {alpha3} </span> </h4>
-                                         <h4>njesiaMonetare: <span> {currencyName} </span> </h4>
+                                         <h4>njësiaMonetare: <span> {currencyName} </span> </h4>
                                        <h4>idMonedhes: <span> {currencyId} </span> </h4>
                                         <h4>simboliMonetar: <span> {currencySymbol} </span> </h4>
                                      </div>
                                 </div>
                           </article>
+                 //   </Link>
             ))}
   
         </section>
