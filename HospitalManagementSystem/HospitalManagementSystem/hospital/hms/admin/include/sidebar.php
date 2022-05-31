10400693ce05189ae8295456fa522daac3f22ca9
Leke Tushaj <lek.tushaj2022@gmail.com>
2022-05-31 14:32:07 +0200

    PROJEKTI NE PHP MYSQL

---
+++ HospitalManagementSystem/HospitalManagementSystem/hospital/hms/admin/include/sidebar.php
@@ -0,0 +1,212 @@
+<div class="sidebar app-aside" id="sidebar">
+				<div class="sidebar-container perfect-scrollbar">
+
+<nav>
+						
+						<!-- start: MAIN NAVIGATION MENU -->
+						<div class="navbar-title">
+							<span>Menyja Kryesore</span>
+						</div>
+						<ul class="main-navigation-menu">
+							<li>
+								<a href="dashboard.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Dashboard </span>
+										</div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Doktorët </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									<li>
+										<a href="doctor-specilization.php">
+											<span class="title"> Shërbimet Specialistike  </span>
+										</a>
+									</li>
+									<li>
+										<a href="add-doctor.php">
+											<span class="title"> Shto Doktorët</span>
+										</a>
+									</li>
+									<li>
+										<a href="Manage-doctors.php">
+											<span class="title"> Menaxhimi Doktorëve </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+    <!--
+				<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Userat </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="manage-users.php">
+											<span class="title"> Menaxhimi userave </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+								<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Pacientët </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="manage-patient.php">
+											<span class="title"> Menaxhimi Pacientëve </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>	
+--> 
+<li>
+								<a href="appointment-history.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-file"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Historia Termineve </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+
+<!--
+				<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-files"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Conatctus Queries </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="unread-queries.php">
+											<span class="title"> Unread Query </span>
+										</a>
+									</li>
+
+									<li>
+										<a href="read-query.php">
+											<span class="title"> Read Query </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+
+
+
+	<li>
+								<a href="doctor-logs.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-list"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Doctor Session Logs </span>
+										</div>
+									</div>
+								</a>
+							</li>
+
+
+
+
+							<li>
+								<a href="user-logs.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-list"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> User Session Logs </span>
+										</div>
+									</div>
+								</a>
+							</li>						
+				<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-files"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Reports </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="between-dates-reports.php">
+											<span class="title">B/w dates reports </span>
+										</a>
+									</li>
+
+									
+									
+								</ul>
+-->
+								<li>
+								<a href="patient-search.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-search"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Kerkimi Pacienteve </span>
+										</div>
+									</div>
+								</a>
+							</li>
+								</li>
+
+						</ul>
+						<!-- end: CORE FEATURES -->
+						
+					</nav>
+					</div>
+			</div>
